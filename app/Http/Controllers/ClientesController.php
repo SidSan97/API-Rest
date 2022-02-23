9b8c56f34b0b8ceb08be3c45a22e7995f79a42f5
SidSan97 <sidnei1.8santiago@hotmail.com>
2022-02-22 21:50:09 -0300

    Ajustes no controller

--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -6,5 +6,41 @@ use Illuminate\Http\Request;
 
 class ClientesController extends Controller
 {
-    //
+    public function index(){
+        $clientes = Clientes::paginate(15);
+        return ClientesResource::collection($clientes);
+      }
+    
+      public function show($id){
+        $clientes = Clientes::findOrFail( $id );
+        return new ClientesResource( $clientes );
+      }
+    
+      public function store(Request $request){
+        $clientes = new Clientes;
+        $clientes->titulo = $request->input('titulo');
+        $clientes->conteudo = $request->input('conteudo');
+    
+        if( $artigo->save() ){
+          return new ClientesResource( $clientes );
+        }
+      }
+    
+       public function update(Request $request){
+        $clientes = Clientes::findOrFail( $request->id );
+        $clientes->titulo = $request->input('titulo');
+        $clientes->conteudo = $request->input('conteudo');
+    
+        if( $clientes->save() ){
+          return new ClientesResource( $clientes );
+        }
+      } 
+    
+      public function destroy($id){
+        $clientes = Clientes::findOrFail( $id );
+        if( $clientes->delete() ){
+          return new ClientesResource( $clientes );
+        }
+    
+      }
 }
