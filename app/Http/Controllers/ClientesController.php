dc0686b50d09012719014cebfbcf0933361fc1ae
SidSan97 <sidnei1.8santiago@hotmail.com>
2022-02-22 23:30:10 -0300

    Adição de arquivos

--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -1,49 +1,90 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
 use App\Models\Clientes as Clientes;
 use App\Http\Resources\Clientes as ClientesResource;
 
 
-use Illuminate\Http\Request;
 
 class ClientesController extends Controller
 {
-    public function index(){
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
         $clientes = Clientes::paginate(15);
         return ClientesResource::collection($clientes);
-      }
-    
-      public function show($id){
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $clientes = new Clientes;
+
+        $clientes->nome  = $request->nome;
+        $clientes->tel   = $request->tel;
+        $clientes->cpf   = $request->cpf;
+        $clientes->placa = $request->placa;
+
+        if( $clientes->save() ){
+        return new ClientesResource( $clientes );
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
         $clientes = Clientes::findOrFail( $id );
         return new ClientesResource( $clientes );
-      }
-    
-      public function store(Request $request){
-        $clientes = new Clientes;
-        $clientes->titulo = $request->input('titulo');
-        $clientes->conteudo = $request->input('conteudo');
-    
-        if( $artigo->save() ){
-          return new ClientesResource( $clientes );
-        }
-      }
-    
-       public function update(Request $request){
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
         $clientes = Clientes::findOrFail( $request->id );
-        $clientes->titulo = $request->input('titulo');
-        $clientes->conteudo = $request->input('conteudo');
-    
+        $clientes->nome  = $request->nome;
+        $clientes->tel   = $request->tel;
+        $clientes->cpf   = $request->cpf;
+        $clientes->placa = $request->placa;
+
         if( $clientes->save() ){
-          return new ClientesResource( $clientes );
-        }
-      } 
-    
-      public function destroy($id){
+        return new ClientesResource( $clientes );
+    }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
         $clientes = Clientes::findOrFail( $id );
         if( $clientes->delete() ){
-          return new ClientesResource( $clientes );
+        return new ClientesResource( $clientes );
         }
-    
-      }
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,18 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientesController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('clientes', [ClientesController::class, 'index']);
+
+Route::get('clientes/{id}', [ClientesController::class, 'show']);
+
+Route::post('clientes', [ClientesController::class, 'store']);
+
+Route::put('clientes/{id}', [ClientesController::class, 'update']);
+
+Route::delete('clientes/{id}', [ClientesController::class,'destroy']);
