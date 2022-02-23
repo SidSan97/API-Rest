c8c07b0fe14c7c7f54a5ec2497df6933721bf157
SidSan97 <sidnei1.8santiago@hotmail.com>
2022-02-22 23:42:28 -0300

    Ajustes de rota

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
-
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientesController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
 });
+
+Route::get('clientes', [ClientesController::class, 'index']);
+
+Route::get('exibir-clientes/{id}', [ClientesController::class, 'show']);
+
+Route::post('criar-cliente', [ClientesController::class, 'store']);
+
+Route::put('editar-clientes/{id}', [ClientesController::class, 'update']);
+
+Route::delete('remover-clientes/{id}', [ClientesController::class,'destroy']);
